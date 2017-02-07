cf1deb243697ebf81245ccbd603db8330c4d7aed
Nensee <nenseekaneriya@gmail.com>
2017-02-07 13:18:24 +0530

    Initial commit

---
+++ app/src/main/java/com/fragment/login/bundlepractical/FirstPage.java
@@ -0,0 +1,11 @@
+package com.fragment.login.bundlepractical;
+
+import android.app.Fragment;
+
+/**
+ * Created by nensee on 2/7/17.
+ */
+public class FirstPage extends Fragment {
+
+
+}
---
+++ app/src/main/java/com/fragment/login/bundlepractical/SecondPage.java
@@ -0,0 +1,9 @@
+package com.fragment.login.bundlepractical;
+
+import android.app.Fragment;
+
+/**
+ * Created by nensee on 2/7/17.
+ */
+public class SecondPage extends Fragment {
+}
